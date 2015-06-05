026e194192945a6e9bb3180b17da53ca40576168
MIRIANOVIEDO <doblem12@hotmail.com>
2015-06-05 10:32:49 -0430

    version05/06/15

---
+++ codigojavascript1.js
@@ -0,0 +1,12 @@
+
+	function mostraralerta()
+		{
+		alert('Hizo clic!');
+		}
+	function hacerclic()
+		{
+		document.getElementsByTagName('p')[0].onclick=mostraralerta;
+		}
+	window.onload=hacerclic
+
+	
